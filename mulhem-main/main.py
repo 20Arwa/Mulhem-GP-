2a51d0cfdea6e2ec26f4af9df36b21cf905e55af
20Arwa <75222830+20Arwa@users.noreply.github.com>
2025-01-11 04:59:14 +0300

    Delete mulhem-main directory

--- mulhem-main/main.py
+++
@@ -1,7 +0,0 @@
-# This File To Run The Server
-from website import create_app # نقدر نسوي امبورت بهذا الشكل لانه اي شي داخل مجلد الويبسايت يصير عبارة عن باكج
-
-app = create_app()
-
-if __name__ == '__main__': # يعني ما يسوي رن للبروجمت إلا إذا سوينا رن دايركت رن لهذا الملف
-    app.run(debug=True)
